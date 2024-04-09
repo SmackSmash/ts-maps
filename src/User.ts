790540a26870b01f497bf20811286d8cbd28b36b
Dan Moffat <danmoffat@gmail.com>
2024-04-09 16:28:34 +0100

    Changed print width from 120 to 80

--- src/User.ts
+++ src/User.ts
@@ -1,5 +1,9 @@
 export default class User {
-  constructor(public firstName: string, public lastName: string, public location: { lat: number; long: number }) {}
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public location: { lat: number; long: number }
+  ) {}
 
   public getFullName(): string {
     return `${this.firstName} ${this.lastName}`;
