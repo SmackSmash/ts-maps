489fa2a9ffc85b4769950b3b10cad96324b98066
Dan Moffat <danmoffat@gmail.com>
2024-04-09 16:44:03 +0100

    Added company class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { faker } from '@faker-js/faker';
 import { User } from './User';
+import { Company } from './Company';
 
 const newUser = new User(faker.person.firstName(), faker.person.lastName(), {
   lat: faker.location.latitude(),
   long: faker.location.longitude()
 });
 
+const newCompany = new Company(faker.company.name());
+
 console.log(newUser.getFullName());
+console.log(newCompany);
