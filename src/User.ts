e8d150ccf08561ce97632eaa3436204e1291958f
Dan Moffat <danmoffat@gmail.com>
2024-04-09 16:22:24 +0100

    Expanded User class

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,7 @@
 export default class User {
-  constructor(public name: string, public location: { lat: number; long: number }) {}
+  constructor(public firstName: string, public lastName: string, public location: { lat: number; long: number }) {}
+
+  public getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { faker } from '@faker-js/faker';
 import User from './User';
 
-const newUser = new User(faker.person.fullName(), { lat: faker.location.latitude(), long: faker.location.longitude() });
+const newUser = new User(faker.person.firstName(), faker.person.lastName(), {
+  lat: faker.location.latitude(),
+  long: faker.location.longitude()
+});
 
-console.log(newUser);
+console.log(newUser.getFullName());
